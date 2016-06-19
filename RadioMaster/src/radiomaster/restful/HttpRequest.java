346491ee968ed66c2139f2f1574540841f8a7b77
SlavenKarakas <skarakas84@gmail.com>
2016-06-19 15:29:04 +0000

    Add files via upload

--- RadioMaster/src/radiomaster/restful/HttpRequest.java
+++ RadioMaster/src/radiomaster/restful/HttpRequest.java
@@ -1,6 +1,18 @@
+package radiomaster.restful;
+
+
 /*
- * /**
-license The MIT License
+
+// * \brief   RESTFUL API HttpRequest
+ * \details This class contains all methods and inner classes
+ * required to execute Get HTTP request towards specific server. Only used for getting data.
+ * 
+ * @author Slaven Karakaš
+ * @version 1.0
+//
+
+
+License The MIT License
 
 Copyright (c) 2012-2016 Gauss, www.gauss.hr
 
@@ -22,72 +34,49 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
  */
-package radiomaster.restful;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.Charset;
 
-/**
- *
- * @author Developer
- */
-public interface HttpRequest {
+public class HttpRequest {
 
-    public static void getData(String string) {
-        if (string.contains("http")) {
-            // TODO: call normal http request execution
+	public static void main(String[] args) throws Exception {
 
-            /**
-             *
-             * @param myURL
-             * @return
-             */
-    public static String executeHTTPRequest(String myURL) {
-        System.out.println("Requested URL:" + myURL);
+		HttpURLConnection http = new HttpURLConnection();
 
-        StringBuilder sb = new StringBuilder();
-        HttpURLConnection urlConn = null;
-        InputStreamReader in = null;
-        try {
-            URL url = new URL(myURL);
-            urlConn = (HttpURLConnection) url.openConnection();
-            if (urlConn != null) {
-                urlConn.setReadTimeout(60 * 1000);
-            }
-            if (urlConn != null && urlConn.getInputStream() != null) {
-                in = new InputStreamReader(urlConn.getInputStream(),
-                        Charset.defaultCharset());
-                try (BufferedReader bufferedReader = new BufferedReader(in)) {
-                    int cp;
-                    while ((cp = bufferedReader.read()) != -1) {
-                        sb.append((char) cp);
-                    }
-                }
+		System.out.println("Testing 1 - Send Http GET request");
+		http.sendGet();
 
-                in.close();
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Exception while calling URL:" + myURL, e);
-        }
+	}
 
-        return sb.toString();
-    }
-}
-else if (string.contains("https")) {
-            // TODO: call https secure protocol request
-        } else {
-            // TODO: handle unused exception
-        }
-    }
+	// HTTP GET request
+	public void sendGet() throws Exception {
 
-    /**
-     * Execute HTTP request to a specified domain
-     *
-     * @param myURL URL of the request
-     * @return String response of the request
-     */
-   
+		String url = "http://radiomaster.gaussx.com/web/app_dev.php/api/stations/list";
+		
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// setting GET
+		con.setRequestMethod("GET");
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
 }
---
+++ RadioMaster/src/radiomaster/restful/HttpsClient.java
@@ -0,0 +1,133 @@
+/*
+
+// * \brief   RESTFUL API HttpsRequest
+ * \details This class contains all methods and inner classes
+ * required to execute Get HTTP request towards specific server. Only used for getting data.
+ * 
+ * @author Slaven Karakaš
+ * @version 1.0
+//
+
+
+License The MIT License
+
+Copyright (c) 2012-2016 Gauss, www.gauss.hr
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+
+
+package radiomaster.restful;
+
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.cert.Certificate;
+import java.io.*;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLPeerUnverifiedException;
+
+public class HttpsClient{
+	
+   public static void main(String[] args)
+   {
+        new HttpsClient().testIt();
+   }
+	
+   private void testIt(){
+
+      String https_url = "http://radiomaster.gaussx.com/web/app_dev.php/api/stations/list";
+      URL url;
+      try {
+
+	     url = new URL(https_url);
+	     HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
+			
+	     //dumpl all cert info
+	     print_https_cert(con);
+			
+	     //dump all the content
+	     print_content(con);
+			
+      } catch (MalformedURLException e) {
+	     e.printStackTrace();
+      } catch (IOException e) {
+	     e.printStackTrace();
+      }
+
+   }
+	
+   private void print_https_cert(HttpsURLConnection con){
+     
+    if(con!=null){
+			
+      try {
+				
+	System.out.println("Response Code : " + con.getResponseCode());
+	System.out.println("Cipher Suite : " + con.getCipherSuite());
+	System.out.println("\n");
+				
+	Certificate[] certs = con.getServerCertificates();
+	for(Certificate cert : certs){
+	   System.out.println("Cert Type : " + cert.getType());
+	   System.out.println("Cert Hash Code : " + cert.hashCode());
+	   System.out.println("Cert Public Key Algorithm : " 
+                                    + cert.getPublicKey().getAlgorithm());
+	   System.out.println("Cert Public Key Format : " 
+                                    + cert.getPublicKey().getFormat());
+	   System.out.println("\n");
+	}
+				
+	} catch (SSLPeerUnverifiedException e) {
+		e.printStackTrace();
+	} catch (IOException e){
+		e.printStackTrace();
+	}
+
+     }
+	
+   }
+	
+   private void print_content(HttpsURLConnection con){
+	if(con!=null){
+			
+	try {
+		
+	   System.out.println("****** Content of the URL ********");			
+	   BufferedReader br = 
+		new BufferedReader(
+			new InputStreamReader(con.getInputStream()));
+				
+	   String input;
+				
+	   while ((input = br.readLine()) != null){
+	      System.out.println(input);
+	   }
+	   br.close();
+				
+	} catch (IOException e) {
+	   e.printStackTrace();
+	}
+			
+       }
+		
+   }
+	
+}
