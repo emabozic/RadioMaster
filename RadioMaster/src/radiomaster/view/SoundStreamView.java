baff3dbd1439a5a1c25fe968b49d9f85d8fef525
Nikolina Pepić <Nikolina Pepi>
2016-07-11 11:24:23 +0200

    soundstream view

--- RadioMaster/src/radiomaster/view/SoundStreamView.java
+++ RadioMaster/src/radiomaster/view/SoundStreamView.java
@@ -5,6 +5,7 @@
  */
 package radiomaster.view;
 
+import java.awt.Color;
 import radiomaster.restful.StreamController;
 
 
@@ -22,6 +23,11 @@ public class SoundStreamView extends javax.swing.JFrame {
      */
     public SoundStreamView() {
         initComponents();
+        setTitle("Stream Player");
+        getContentPane().setBackground(Color.white);
+        
+        
+       
     }
 
     /**
@@ -36,6 +42,8 @@ public class SoundStreamView extends javax.swing.JFrame {
         txtURL = new javax.swing.JTextField();
         btnPlay = new javax.swing.JButton();
         btnStop = new javax.swing.JButton();
+        lblPlay = new javax.swing.JLabel();
+        lblStop = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -55,6 +63,22 @@ public class SoundStreamView extends javax.swing.JFrame {
             }
         });
 
+        lblPlay.setBackground(new java.awt.Color(255, 255, 255));
+        lblPlay.setIcon(new javax.swing.ImageIcon(getClass().getResource("/radiomaster/icons/Play1Normal.png"))); // NOI18N
+        lblPlay.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                lblPlayMouseReleased(evt);
+            }
+        });
+
+        lblStop.setBackground(new java.awt.Color(255, 255, 255));
+        lblStop.setIcon(new javax.swing.ImageIcon(getClass().getResource("/radiomaster/icons/Button%20White%20Stop.png"))); // NOI18N
+        lblStop.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                lblStopMouseReleased(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -66,17 +90,30 @@ public class SoundStreamView extends javax.swing.JFrame {
                     .addComponent(btnPlay, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(btnStop, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(153, 153, 153)
+                .addComponent(lblPlay, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblStop, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(37, 37, 37)
                 .addComponent(txtURL, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(33, 33, 33)
+                        .addComponent(lblStop, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(lblPlay, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(75, 75, 75)
                 .addComponent(btnPlay)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(btnStop)
-                .addContainerGap(201, Short.MAX_VALUE))
+                .addContainerGap(36, Short.MAX_VALUE))
         );
 
         pack();
@@ -86,6 +123,7 @@ public class SoundStreamView extends javax.swing.JFrame {
 
         sc.Play(txtURL.getText());
         
+        
 
     }//GEN-LAST:event_btnPlayActionPerformed
 
@@ -94,11 +132,23 @@ public class SoundStreamView extends javax.swing.JFrame {
         sc.Stop();
     }//GEN-LAST:event_btnStopActionPerformed
 
+    private void lblPlayMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblPlayMouseReleased
+        // TODO add your handling code here:
+        sc.Play(txtURL.getText());
+    }//GEN-LAST:event_lblPlayMouseReleased
+
+    private void lblStopMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblStopMouseReleased
+        // TODO add your handling code here:
+        sc.Stop();
+    }//GEN-LAST:event_lblStopMouseReleased
+
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnPlay;
     private javax.swing.JButton btnStop;
+    private javax.swing.JLabel lblPlay;
+    private javax.swing.JLabel lblStop;
     private javax.swing.JTextField txtURL;
     // End of variables declaration//GEN-END:variables
 }
